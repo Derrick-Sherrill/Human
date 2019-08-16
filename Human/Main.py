b82b36c517658d7f2fd87bb7b9aeaeaab8d746b0
Henry V. Gilbert <henrygilbert609@gmail.com>
2019-08-15 21:48:26 -0400

    Add files via upload

---
+++ Human/Main.py
@@ -0,0 +1,41 @@
+'''
+Human body model system: attempt to use OOP to model the human body.
+Includes all vital functions for survival!
+
+Created by: Henry Gilbert
+Date: 8/16/2019
+
+Project organization:
+- human is an instance and has class methods along with instance methods.
+- The systems are a seperate class that the human inherits along with its other
+  subclasses.
+
+Human body main systems:
+- Digestive system
+- Circulatory system
+- Endocrine system
+- Immune system
+- Integumentary system
+- Immune system
+- Muscular system
+- Reproductive system
+- Respiratory system
+- Skeletal system
+- Nervous system
+'''
+
+class Human:
+	# Instance of human pulls data from all systems to establish baseline homeostasis and
+	# alive status. Has built in feedback loops to redirect back to homeostasis. All objects
+	# and systems can communicate with each other.
+	alive = True
+
+	def __init__(self,):
+		pass
+
+
+
+
+
+	pass
+
---
+++ Human/System.py
@@ -0,0 +1,10 @@
+class System:
+
+	pass
+
+
+
+
+
+
+
